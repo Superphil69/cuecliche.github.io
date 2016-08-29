594527d86f766dfc6d0c604fe6d6a3bd314e7978
Phil Wrigglesworth <phil_wriggy@icloud.com>
2016-08-29 10:44:51 +0200

    touch scrolling

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -15,6 +15,6 @@ $(document).ready(function() {
 $(document).ready(function() {
     $('.hover').bind('touchstart touchend', function(e) {
         e.preventDefault();
-        $(this).toggleClass('hover_effect');
+        $(this).toggleClass('reminders');
     });
 });
