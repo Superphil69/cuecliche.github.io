c27131d77f61fa0d9a74705697b4a6e72a8cc574
Phil Wrigglesworth <phil_wriggy@icloud.com>
2016-10-18 14:35:21 +0200

    Cuecliché site to go live

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,20 +1,45 @@
-$(document).ready(function() {
-  $('.open_curtain').click(function(){
-    $("#curtain1").animate({
-      width:20}
-      ,1000);
-    $("#curtain2").animate({width:20},1000);
+$(function() {
+	clientStuff();
+});
+
+function clientStuff() {
+
+  $('.pic-reel, .pic-thumb').click(function() {
+    var $this = $(this),
+        position = $this.parent().children().index($this);
+
+    $('.pic-reel').removeClass('active-client').eq(position).addClass('active-client');
+    $('.pic-thumb').removeClass('active-client').eq(position).addClass('active-client');
   });
 
- $('.close_curtain').click(function(){
-   $("#curtain1").animate({width:230},1000);
-   $("#curtain2").animate({width:220},1000);
- });
-});
 
-$(document).ready(function() {
-    $('.hover').bind('touchstart touchend', function(e) {
-        e.preventDefault();
-        $(this).toggleClass('reminders');
-    });
-});
+  $('.pic-control-next, .pic-control-prev').click(function() {
+
+    var $this = $(this),
+        curActiveClient = $('.pic-belt').find('.active-client'),
+        position = $('.pic-belt').children().index(curActiveClient),
+        clientNum = $('.pic-reel').length;
+
+      if($this.hasClass('pic-control-next')) {
+
+        if(position < clientNum -1){
+          $('.active-client').removeClass('active-client').next().addClass('active-client');
+        } else {
+          $('.pic-reel').removeClass('active-client').first().addClass('active-client');
+          $('.pic-thumb').removeClass('active-client').first().addClass('active-client');
+        }
+
+      } else {
+
+        if (position === 0) {
+          $('.pic-reel').removeClass('active-client').last().addClass('active-client');
+          $('.pic-thumb').removeClass('active-client').last().addClass('active-client');
+        } else {
+          $('.active-client').removeClass('active-client').prev().addClass('active-client');
+        }
+
+      }
+
+  });
+
+}
